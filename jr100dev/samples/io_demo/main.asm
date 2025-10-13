fa66558f98cb4edcda9cf60184b222e70b96a18f
Zabaglione <z.zabaglione@gmail.com>
2025-10-13 18:39:58 +0900

    Expand IO demo to show key codes

--- jr100dev/samples/io_demo/main.asm
+++ jr100dev/samples/io_demo/main.asm
@@ -5,10 +5,42 @@
 
 MAIN:   BEEP
         LDX #STD_VRAM_BASE
+        PRINT_STR TITLE
+        LDX #STD_VRAM_BASE + 32
         PRINT_STR PROMPT
-WAIT:   SCAN_KEY
-        CMPA #$00
-        BEQ WAIT
-        BRA MAIN
+        LDX #STD_VRAM_BASE + 64
+        PRINT_STR LABEL
+        LDX #STD_VRAM_BASE + 96
+        PRINT_STR KEYMAP
+        LDX #STD_VRAM_BASE + 128
+        PRINT_STR KEYMAP2
+        LDX #STD_VRAM_BASE + 32
 
-PROMPT: .ascii "PRESS ANY KEY\0"
+INPUT_LOOP:
+        SCAN_KEY
+        CMPA #$00
+        BEQ INPUT_LOOP
+
+        STAA LAST_KEY
+        PLA
+
+SHOW_KEY:
+        LDX #STD_VRAM_BASE + 96 + 16
+        LDAA LAST_KEY
+        JSR __STD_TO_VRAM
+        STAA ,X
+
+        LDX #STD_VRAM_BASE + 64
+        PRINT_STR LABEL
+        LDX #STD_VRAM_BASE + 96
+        PRINT_STR KEYMAP
+        LDX #STD_VRAM_BASE + 128
+        PRINT_STR KEYMAP2
+        BRA INPUT_LOOP
+
+TITLE:  .ascii "JR-100 I/O DEMO\0"
+PROMPT: .ascii "PRESS ANY KEY TO SEE CODE\0"
+LABEL:  .ascii "LAST KEY CODE:\0"
+KEYMAP: .ascii "0-9 -> 00-09  A-F -> 0A-0F\0"
+KEYMAP2:.ascii "START/STOP -> 1C   SPACE -> 01\0"
+LAST_KEY: .byte $00
